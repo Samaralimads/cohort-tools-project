939ec29b7fa51cbdb9f896720d0d8b7211ece765
Maria Frih <maria.frih@gmail.com>
2024-01-29 16:57:24 +0100

    imports and sets up cors middleware

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
+const cors = require("cors");
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
@@ -14,7 +15,7 @@ const cohorts = require("./cohorts.json");
 const students = require("./students.json");
 
 // MIDDLEWARE
-// Research Team - Set up CORS middleware here:
+app.use(cors({ origin: ["http://localhost:5173"] }));
 // ...
 app.use(express.json());
 app.use(morgan("dev"));
