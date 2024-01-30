149a311af14b92d74d1c21e7c539082b4fe4f60a
“Samara <samara.limads@gmail.com>
2024-01-30 17:00:44 +0100

    added Mongoose and cohort Schema

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
@@ -10,6 +11,11 @@ const cors = require("cors");
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/mongoose-example-dev")
+  .then((x) => console.log(`Connected to Database: "${x.connections[0].name}"`))
+  .catch((err) => console.error("Error connecting to MongoDB", err));
 
 const cohorts = require("./cohorts.json");
 const students = require("./students.json");
